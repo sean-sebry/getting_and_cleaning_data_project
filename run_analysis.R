587f79a12e5bde99a4c6304ef102825f7cf47d81
Sean Sebry <sean.sebry@gmail.com>
2019-11-08 19:09:02 +0000

    work progress

--- run_analysis.R
+++ run_analysis.R
@@ -24,20 +24,23 @@ comb_train_df <- cbind.data.frame(subject_train_df,y_train_df,x_train_df)
 
 #combine for tidy data set
 tidy_full_data_set <- rbind(comb_test_df, comb_train_df)
-glimpse(tidy_full_data_set)
 
-#eda and treating of dataset, first pass
-names <- colnames(tidy_full_data_set) %>% print()
-unique(names)#Seems to have duplicates
+
+#eda and treating of dataset
+names <- colnames(tidy_full_data_set)
 select_unique<-!duplicated(names)
-select_unique
-tidy_treated_data_set <- as.data.frame(tidy_full_data_set[,select_unique==TRUE])#removing duplicate columns
-tidy_treated_data_set <- tidy_treated_data_set %>% rename(y_activity = y)#adding a more descriptive name to 'y'
+tidy_treatment_1_data_set <- as.data.frame(tidy_full_data_set[,select_unique==TRUE])#removing duplicate columns
+tidy_treatment_1_data_set <- tidy_treatment_1_data_set %>% rename(y_activity = y)#adding a more descriptive name to 'y'
+activity_labels <- read_table(file = "/cloud/project/UCI HAR Dataset/activity_labels.txt",col_names = FALSE)#bringing in activity labels
+
+tidy_treatment_2_data_set <- tidy_treatment_1_data_set %>%
+  mutate(y_activity = case_when(
+    y_activity == 1 ~ as.character(activity_labels[1, 2]),
+    y_activity == 2 ~ as.character(activity_labels[2, 2]),
+    y_activity == 3 ~ as.character(activity_labels[3, 2]),
+    y_activity == 4 ~ as.character(activity_labels[4, 2]),
+    y_activity == 5 ~ as.character(activity_labels[5, 2]),
+    y_activity == 6 ~ as.character(activity_labels[6, 2])))
+glimpse(tidy_treatment_2_data_set)
 
 
-
-#Apply activity labels
-activity_labels <- read_table(file = "/cloud/project/UCI HAR Dataset/activity_labels.txt",col_names = FALSE)
-
-
-activity_labels
